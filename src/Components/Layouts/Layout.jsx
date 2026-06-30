9a36035a2a8b860f236d617cf8a776bbb8ab865f
yasmin-jahan <yasminjahan8603@gmail.com>
2026-06-30 15:31:56 +0530

    Add files via upload

--- src/Components/Layouts/Layout.jsx
+++ src/Components/Layouts/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
 
 function Layout({children}) {
   return (
